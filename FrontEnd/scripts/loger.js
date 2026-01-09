0f77d406eee2b1de8ef9eb2b6e01855845868606
delby3006-del <delby3006@gmail.com>
2026-01-09 15:20:51 +0100

    import photo + suppression (poubelle sur photo) de la modale .js

--- FrontEnd/scripts/loger.js
+++ FrontEnd/scripts/loger.js
@@ -35,18 +35,60 @@ async function ajouterBordureAdmin() {
   document.querySelector(".border-admin").appendChild(modeEdition);
 }
 
+async function supprimerphotoModale(id) {
+  const response = await fetch("http://localhost:5678/api/works/" + id, {
+    method: "delete",
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
+  });
+}
+
+async function afficherGaleriesModale() {
+  const response = await fetch("http://localhost:5678/api/works");
+  const works = await response.json();
+  console.log(works);
+
+  for (let i = 0; i < works.length; i++) {
+    const work = works[i];
+    const imageElement = document.createElement("img");
+    imageElement.className = "photos-modale";
+    imageElement.src = work.imageUrl;
+
+    const figureElement = document.createElement("figure");
+    figureElement.className = "figureElement";
+
+    const poubelle = document.createElement("button");
+    poubelle.className = "fa-solid fa-trash-can icone-poubelle";
+    poubelle.addEventListener("click", async () => {
+      await supprimerphotoModale(work.id);
+      figureElement.remove();
+    });
+    document.querySelector(".afficher-photo").appendChild(figureElement);
+    figureElement.appendChild(imageElement);
+    figureElement.appendChild(poubelle);
+  }
+}
+
 async function créerModaleAdmin() {
+  afficherGaleriesModale();
   const modaleAdmin = document.createElement("dialog");
   const croixmodale = document.createElement("i");
   const titreModale = document.createElement("h2");
+  const afficherphoto = document.createElement("div");
+  const ajouterphoto = document.createElement("button");
   titreModale.className = "titre-modale";
-  titreModale.innerText = "Galerie photo";
   croixmodale.className = "fa-solid fa-xmark croix-modale";
   modaleAdmin.className = "modale-admin";
-  // modaleAdmin.innerHTML = "test modale admin";
+  afficherphoto.className = "afficher-photo";
+  ajouterphoto.className = "ajouter-photo";
+  titreModale.innerText = "Galerie photo";
+  ajouterphoto.innerText = "Ajouter une photo";
   document.querySelector("body").prepend(modaleAdmin);
   document.querySelector(".modale-admin").prepend(croixmodale);
   document.querySelector(".modale-admin").prepend(titreModale);
+  document.querySelector(".modale-admin").appendChild(afficherphoto);
+  document.querySelector(".modale-admin").appendChild(ajouterphoto);
 }
 
 async function creerBoutonModificerProjetsAdmin() {
